fba408dd7a9a311dfe1fcb3c5cf75224c89a112d
12FourOnSix <fouronsix@yandex.ru>
2022-05-23 23:34:25 +0300

    feat: add solution for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const { copyFile, mkdir, readdir } = require('fs/promises');
+const { join } = require('path');
 
+async function init() {
+  try {
+    const copyDirPath = join(__dirname, 'files-copy');
+    await mkdir(copyDirPath, { recursive: true});
+
+    const origDirPath = join(__dirname, 'files');
+    const filesNamesList = await readdir(origDirPath);
+    
+    for (const fileName of filesNamesList) {
+      const origFilePath = join(origDirPath, fileName);
+      const copyFilePath = join(copyDirPath, fileName);
+
+      await copyFile(origFilePath, copyFilePath);
+    }
+
+  } catch (err) {
+    console.error('Содержимое исходной папки не может быть скопировано: ', err.message);
+  }
+}
+
+init();
